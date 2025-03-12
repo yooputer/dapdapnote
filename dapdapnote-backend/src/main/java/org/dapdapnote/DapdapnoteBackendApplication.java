0ec1cf7190aeeeccc69146178b528f58e220c7cb
김유진 <yoojin027@mteletec.com>
2025-03-12 13:15:55 +0900

    add files

---
+++ dapdapnote-backend/src/main/java/org/dapdapnote/DapdapnoteBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dapdapnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapdapnoteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DapdapnoteBackendApplication.class, args);
+    }
+
+}
---
+++ dapdapnote-backend/src/main/java/org/dapdapnote/TestController.java
@@ -0,0 +1,14 @@
+package org.dapdapnote;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("")
+    public String index() {
+        return "hi~";
+    }
+
+}
