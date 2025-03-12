559012240ecfde7abaaa4fb89bf2c3f5a01bce97
김유진 <yoojin027@mteletec.com>
2025-03-12 13:24:11 +0900

    init

---
+++ dapdapnote-front/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8085',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
