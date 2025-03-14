aefd370dac4404f240edf9c54deca4c889a3ed09
김유진 <yoojin027@mteletec.com>
2025-03-14 16:42:47 +0900

    🟣chore: vue 개발서버 프록시 설정

--- dapdapnote-front/vue.config.js
+++ dapdapnote-front/vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8085',
+            changeOrigin: true
+      }
+    }
+  }
+}
